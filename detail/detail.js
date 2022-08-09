27778b83ae3d6de8e5f17305d9281adb64e289b1
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-08 17:14:28 -0700

    add functionality to the webpage by displaying a list of dogs with thier image to the main page. Allows user to click on image to go to a detailed page.

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,15 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
+const params = new URLSearchParams(window.location.search);
+
+async function loadData() {
+    const data = await getDog(params.get('id'));
+    const dogDiv = renderDogDetail(data);
+    dogDetailContainer.append(dogDiv);
+}
+loadData();
